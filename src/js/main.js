4f59d4f3f748fe2d85e0785532d358b2f73a7376
Vlad Tepes <vg.email.2@gmail.com>
2018-04-24 20:03:23 +0300

    scripting finished

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,36 @@ $(document).ready(function () {
     (function () {
         svg4everybody();
     }());
+    
+    //----------------------<<checkbox>>----------------------\\
+    (function () {
+        var flag = true;
+
+        $('.courses-item').on('click', function (e) {
+            e.preventDefault();
+
+            var $this = $(this);
+
+            if(flag){
+                flag = false;
+
+                if ($this.hasClass('checked')){
+                    $this.removeClass('checked');
+                } else{
+                    $this.addClass('checked').children().delay(1000).slideUp(2000, function(){
+                        $this.empty().animate({width: 0}, 1000, function(){
+                                $(this).remove();
+                            });
+                        flag = true;
+
+                        if($('.courses-item').length < 2) {
+                            $('.section2__top').fadeOut(1000, function () {
+                                $('.completed').fadeIn(1000);
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    }());
 });
